500142b74f244a1763ec295cba43290f31f4c24b
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-09-25 20:26:54 +0530

    Update 29.py

--- 29.py
+++ 29.py
@@ -1,3 +1,4 @@
 # What relationship is appropriate for Student and Person? 
 
-# Subclass
+# This demonstrates the hierarchical relationship between the Student and Person classes
+# where Student inherits attributes and methods from Person and adds its own specific attributes and methods.
