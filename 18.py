7c97912c0d1cb050d3601887dccc81d2a1907f51
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-09-25 20:41:42 +0530

    Update 18.py

--- 18.py
+++ 18.py
@@ -1,3 +1,4 @@
 # What happens when „1‟== 1 is executed?
 
-# False
+ # Python uses strict type checking for equality comparisons. This means that the values must be of the same data type to be considered equal.
+# Since the data types are different, the comparison fails, and the expression evaluates to False
