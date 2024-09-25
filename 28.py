50f3b079ed172c80926e7860f643fb1f908f536e
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-09-25 20:28:36 +0530

    Update 28.py

--- 28.py
+++ 28.py
@@ -1,3 +1,4 @@
 # What relationship is appropriate for Course and Faculty?
 
-#One-to-Many
+# The Course class has a faculty attribute to store the Faculty object that teaches the course.
+# The Faculty class has a courses attribute to store a list of Course objects that the faculty teaches.
