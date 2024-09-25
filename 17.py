e6f0fc9acd695f9e52648913975a4bf3db33281a
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-09-25 20:45:06 +0530

    Update 17.py

--- 17.py
+++ 17.py
@@ -1,3 +1,5 @@
 # When is the finally block executed?
 
+# The finally block in Python is always executed, regardless of whether an exception occurs or not. 
+# It's a crucial part of exception handling that ensures certain code is run, even if an exception is raised and not caught.
 # yes
