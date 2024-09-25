868cd0b2e4f8e4fec7928de0dfc62114dbd032d9
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-09-25 20:47:18 +0530

    Update 16.py

--- 16.py
+++ 16.py
@@ -1,3 +1,4 @@
 # Can one block of except statements handle multiple exception? 
-
+# one block of except statements can handle multiple exceptions in Python.
+# You can specify multiple exceptions in a single except clause using parentheses and the or operator.
 # Yes 
