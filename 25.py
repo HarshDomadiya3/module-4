58a8d30992a41c201da1c9755b98d02b247b9275
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-09-25 20:38:44 +0530

    Update 25.py

--- 25.py
+++ 25.py
@@ -1,4 +1,22 @@
-#  What is init? Or What Is A Constructor In Python? 
+#  Explain Inheritance in Python with an example .What is init? Or What Is A Constructor In Python? 
+
+class Animal:
+    def __init__(self,name):
+        self.name=name
+    def speak(self):
+        print("animal sound")
+class Dog(Animal):
+    def speak(self):
+        print("Woof!")
+class Cat(Animal):
+    def speak(self):
+        print("Meow!")
+animal1 =Animal("Animal")
+dog1 =Dog("Buddy")
+cat1 =Cat("Whiskers")
+animal1.speak()
+dog1.speak()
+cat1.speak()
 
 #  __init__ :- is a special method known as the constructor. It is used to initialize the attributes of a class when an instance (object) of the class is created.
 
