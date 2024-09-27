8f6f6f4d873372be9f166954eba1a8febeb4fcf0
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-09-27 20:41:12 +0530

    Update 29.py

--- 29.py
+++ 29.py
@@ -1,4 +1,11 @@
 # What relationship is appropriate for Student and Person? 
-
+class Person:
+    def __init__(self,name):
+        self.name=name
+class Student(Person):
+    def __init__(self, name):
+        super().__init__(name)
+student1=Student("harsh")
+print(student1.name) 
 # This demonstrates the hierarchical relationship between the Student and Person classes
 # where Student inherits attributes and methods from Person and adds its own specific attributes and methods.
