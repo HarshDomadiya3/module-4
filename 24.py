0a839b669676e1a86662e4f32b09cd7e8d7e6ab2
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-10-02 21:10:15 +0530

    Update 24.py

--- 24.py
+++ 24.py
@@ -7,9 +7,12 @@ class Circle:
     def __init__(self,radius):
         self.radius=radius
     def area(self):
+        
         return math.pi *self.radius *self.radius
     def perimeter(self):
         return 2 *math.pi*self.radius
+        
 circle=Circle(5)
+
 print("Area:",circle.area())         
 print("Perimeter:",circle.perimeter())
