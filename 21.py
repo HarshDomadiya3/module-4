427225dd63ddf48f36cca5cc828b8e76ab383ce7
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-10-02 21:02:50 +0530

    Update 21.py

--- 21.py
+++ 21.py
@@ -1,8 +1,21 @@
 # What are oops concepts? Is multiple inheritance supported in java
+Class:-
+        A class is a blueprint for creating objects. It defines attributes (data) and methods (functions) that its objects will have.
 
-# Encapsulation
-# Inheritance
-# Polymorphism
-# Abstraction
+Object:-
+        An object is an instance of a class. Once a class is defined, objects can be created with specific data and behaviors.
+          
+Encapsulation:-
+                Encapsulation is the concept of bundling the data (attributes) and the methods that operate on the data into a single unit or class, 
+                and restricting access to some of the object's components.
 
-# supported in java:- No
+Inheritance:-
+                Inheritance allows one class to inherit the properties and methods of another class. It helps in code reusability and creating hierarchical relationships.
+
+Polymorphism:-
+                Polymorphism allows methods to do different things based on the object they are acting upon, even though they share the same name.
+
+Abstraction:-
+                Abstraction is the concept of hiding the complex implementation details and showing only the essential features of the object.
+                  
+ supported in java:- No
