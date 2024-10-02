26774cebb56edf4196e5a06c273ebc17f6339873
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-10-02 20:53:21 +0530

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,4 +1,11 @@
 # Write a Python program to read last n lines of a file. 
 
+file = 'examplefile.txt'
+n = 5 
+
+with open(file,'r') as file:
+    lines = file.readlines()
+  
+print(''.join(lines[-n:]))
 
 # file.lines[-n:]
