f5d06557fc3f2b9a32bcd31a91310b7841a58cb4
HarshDomadiya3 <harshdomadiya7@gmail.com>
2024-10-02 21:15:20 +0530

    Update 28.py

--- 28.py
+++ 28.py
@@ -1,16 +1,20 @@
 # What relationship is appropriate for Course and Faculty?
-class Course:
-    def __init__(self,name):
-        self.name=name
-        self.faculty=[] 
 class Faculty:
-    def __init__(self,name):
-        self.name=name
-course1=Course("Python")
-course1.faculty.append("Harsh")
-course1.faculty.append("Domadiya")
-print("Course:", course1.name)
-print("Faculty Members:",",".join(course1.faculty))
+    def __init__(self, name):
+        self.name = name
+
+class Course:
+    def __init__(self, title, faculty):
+        self.title = title
+        self.faculty = faculty 
+
+
+professor = Faculty("harsh")
+course = Course("Computer", professor)
+
+print(course.title ,course.faculty.name)
+
+
 
 # The Course class has a faculty attribute to store the Faculty object that teaches the course.
 # The Faculty class has a courses attribute to store a list of Course objects that the faculty teaches.
